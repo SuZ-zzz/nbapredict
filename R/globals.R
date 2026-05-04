89e8386b66c383d2162d5bbc57fc7f687283f5f8
yoshino12326 <1098730719@qq.com>
2026-05-04 16:16:04 +0800

    Add global column names for dplyr usage
    This file declares global column names used in dplyr code for data manipulation.

---
+++ R/globals.R
@@ -0,0 +1,23 @@
+# This file declares column names used in dplyr code.
+utils::globalVariables(c(
+  "Season", "Player", "Pos", "Age", "Tm",
+  "G", "GS", "MP",
+  "FG", "FGA", "FG%",
+  "3P", "3PA", "3P%",
+  "2P", "2PA", "2P%",
+  "eFG%",
+  "FT", "FTA", "FT%",
+  "ORB", "DRB", "TRB",
+  "AST", "STL", "BLK",
+  "TOV", "PF", "PTS",
+  "season", "player_name", "position", "age", "team",
+  "games_played", "games_started", "minutes_played",
+  "field_goals", "field_goal_attempts", "field_goal_pct",
+  "three_points", "three_point_attempts", "three_point_pct",
+  "two_points", "two_point_attempts", "two_point_pct",
+  "effective_field_goal_pct",
+  "free_throws", "free_throw_attempts", "free_throw_pct",
+  "offensive_rebounds", "defensive_rebounds", "total_rebounds",
+  "assists", "steals", "blocks",
+  "turnovers", "personal_fouls", "points", "ppg"
+))
